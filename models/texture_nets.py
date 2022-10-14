b7d0ce5dd26e010cebc158d9680c8395e56f132a
thomas <tdevasia1997@gmail.com>
2022-10-14 04:37:14 +0100

    update

--- models/texture_nets.py
+++ models/texture_nets.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from .common import * 
+from .common import *
 
 
 normalization = nn.BatchNorm2d
@@ -12,17 +12,18 @@ def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero'):
     elif pad == 'reflection':
         layers = [nn.ReflectionPad2d((kernel_size - 1) / 2),
                   nn.Conv2d(in_f, out_f, kernel_size, stride, padding=0, bias=bias)]
-        return nn.Sequential(*layers)
+        # return nn.Sequential(*layers)
+        return nn.Conv2d(in_f, out_f, kernel_size, stride, padding=0, bias=bias)
 
-def get_texture_nets(inp=3, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False, pad='zero', need_sigmoid=False, conv_num=8, upsample_mode='nearest'):
 
+def get_texture_nets(inp=3, ratios=[32, 16, 8, 4, 2, 1], fill_noise=False, pad='zero', need_sigmoid=False, conv_num=8, upsample_mode='nearest'):
 
     for i in range(len(ratios)):
         j = i + 1
 
         seq = nn.Sequential()
 
-        tmp =  nn.AvgPool2d(ratios[i], ratios[i])
+        tmp = nn.AvgPool2d(ratios[i], ratios[i])
 
         seq.add(tmp)
         if fill_noise:
@@ -49,7 +50,7 @@ def get_texture_nets(inp=3, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False, pad
 
             cur = nn.Sequential()
 
-            # Batch norm before merging 
+            # Batch norm before merging
             seq.add(normalization(conv_num))
             cur_temp.add(normalization(conv_num * (j - 1)))
 
@@ -67,11 +68,11 @@ def get_texture_nets(inp=3, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False, pad
             cur.add(normalization(conv_num * j))
             cur.add(act())
 
-            if i == len(ratios) - 1: 
+            if i == len(ratios) - 1:
                 cur.add(conv(conv_num * j, 3, 1, pad=pad))
             else:
-                cur.add(nn.Upsample(scale_factor=2, mode=upsample_mode)) 
-            
+                cur.add(nn.Upsample(scale_factor=2, mode=upsample_mode))
+
     model = cur
     if need_sigmoid:
         model.add(nn.Sigmoid())
