275dc400e436075d090a29a07d39ef376ff5557c
thomas <tdevasia1997@gmail.com>
2022-10-14 02:57:53 +0100

    init add unet n output channel

--- models/__init__.py
+++ models/__init__.py
@@ -25,7 +25,7 @@ def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='Le
                                32, 16, 8, 4, 2, 1], fill_noise=False, pad=pad)
 
     elif NET_TYPE == 'UNet':
-        net = UNet(num_input_channels=input_depth, num_output_channels=3,
+        net = UNet(num_input_channels=input_depth, num_output_channels=n_channels,
                    feature_scale=4, more_layers=0, concat_x=False,
                    upsample_mode=upsample_mode, pad=pad, norm_layer=nn.BatchNorm2d, need_sigmoid=True, need_bias=True)
     elif NET_TYPE == 'identity':
